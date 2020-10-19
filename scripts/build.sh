d6353b5b627e901be9ceab7fa91c5ae8bf246f19
PovilasU <PovilasU@users.noreply.github.com>
2020-10-19 18:12:06 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@
 #sudo docker build -t localhost:8082/service_3 ./service-3
 #sudo docker build -t localhost:8082/service_4 ./service-4
 
-sudo docker build -t localhost:8082/service_1:${BUILD_NUMBER} ./service-1:${BUILD_NUMBER}
-sudo docker build -t localhost:8082/service_2:${BUILD_NUMBER} ./service-2:${BUILD_NUMBER}
-sudo docker build -t localhost:8082/service_3:${BUILD_NUMBER} ./service-3:${BUILD_NUMBER}
-sudo docker build -t localhost:8082/service_4:${BUILD_NUMBER} ./service-4:${BUILD_NUMBER}
+sudo docker build -t localhost:8082/service_1:${BUILD_NUMBER} ./service-1
+sudo docker build -t localhost:8082/service_2:${BUILD_NUMBER} ./service-2
+sudo docker build -t localhost:8082/service_3:${BUILD_NUMBER} ./service-3
+sudo docker build -t localhost:8082/service_4:${BUILD_NUMBER} ./service-4
