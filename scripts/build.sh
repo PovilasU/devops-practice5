6abad4e05f5843f05a6e6c6d234b1f7ff3a4fed9
PovilasU <PovilasU@users.noreply.github.com>
2020-10-19 17:45:04 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 sudo docker build -f service-1/Dockerfile -t localhost:8082/service_1  . 
 #sudo docker build -f service-2/Dockerfile -t localhost:8082/service_2  .
 
-#sudo docker build -t localhost:8082/service_2 ./service-2
+sudo docker build -t localhost:8082/service_2 ./service-2
