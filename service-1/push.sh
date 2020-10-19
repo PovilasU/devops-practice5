2fe15ec56f23d43e66d044ad21a076e7a6f45cc8
PovilasU <PovilasU@users.noreply.github.com>
2020-10-19 23:10:16 +0200

    Delete push.sh

--- service-1/push.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-#sudo docker push localhost:8082/service_1
-#sudo docker pull localhost:8082/service_1 
-
-sudo docker push localhost:8082/service_1:${BUILD_NUMBER} 
-#sudo docker pull localhost:8082/service_1:${BUILD_NUMBER} 
-
