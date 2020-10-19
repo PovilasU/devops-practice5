27b53c6e158a13376df21aa8dd906e532653ae62
PovilasU <PovilasU@users.noreply.github.com>
2020-10-19 20:37:19 +0200

    Update push.sh

--- service-1/push.sh
+++ service-1/push.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
-sudo docker push localhost:8082/service_1
-sudo docker pull localhost:8082/service_1 
+#sudo docker push localhost:8082/service_1
+#sudo docker pull localhost:8082/service_1 
+
+sudo docker push localhost:8082/service_1:${BUILD_NUMBER} 
+sudo docker pull localhost:8082/service_1:${BUILD_NUMBER} 
+
