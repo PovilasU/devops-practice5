c000067e9e0036227a1e59479c60adc6227f0def
PovilasU <PovilasU@users.noreply.github.com>
2020-10-19 22:58:12 +0200

    Update push_services.sh

--- scripts/push_services.sh
+++ scripts/push_services.sh
@@ -1,4 +1,14 @@
-sh "./service-1/push.sh"
+#! /bin/bash
+#sudo docker push localhost:8082/service_1
+#sudo docker pull localhost:8082/service_1 
+
+sudo docker push localhost:8082/service_1:${BUILD_NUMBER}
+sudo docker push localhost:8082/service_2:${BUILD_NUMBER}
+sudo docker push localhost:8082/service_3:${BUILD_NUMBER}
+sudo docker push localhost:8082/service_4:${BUILD_NUMBER}
+
+
+#sh "./service-1/push.sh"
 #sh "./service-2/push.sh"
 #sh "./service-3/push.sh"
 #sh "./service-4/push.sh"
