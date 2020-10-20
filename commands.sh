0d78b1f7f90597d9b6cd8f17e273551b640de29e
PovilasU <PovilasU@users.noreply.github.com>
2020-10-20 10:35:31 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -7,3 +7,9 @@ pip install -U docker-compose
 
 #step3 check docker-compose validity
 docker-compose config
+
+
+#Command 'docker-compose' not found, but can be installed with:
+
+#sudo snap install docker          # version 19.03.11, or
+#sudo apt  install docker-compose
