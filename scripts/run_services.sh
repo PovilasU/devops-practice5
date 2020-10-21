7b2b94a7b9b90963393e74ee6696a4723f524c26
PovilasU <PovilasU@users.noreply.github.com>
2020-10-21 19:14:06 +0200

    Update run_services.sh

--- scripts/run_services.sh
+++ scripts/run_services.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo docker run -d -p 5000:5000 --name service_1 --network kotik_network:${BUILD_NUMBER} localhost:8082/service_1:${BUILD_NUMBER} 
-sudo docker run -d -p 5001:5001 --name service_2 --network kotik_network:${BUILD_NUMBER} localhost:8082/service_2:${BUILD_NUMBER}
-sudo docker run -d -p 5002:5002 --name service_3 --network kotik_network:${BUILD_NUMBER} localhost:8082/service_3:${BUILD_NUMBER}
-sudo docker run -d -p 5003:5003 --name service_4 --network kotik_network:${BUILD_NUMBER} localhost:8082/service_4:${BUILD_NUMBER}
+sudo docker run -d -p 5000:5000 --name service1 --network kotik_network:${BUILD_NUMBER} localhost:8082/service1:${BUILD_NUMBER} 
+sudo docker run -d -p 5001:5001 --name service2 --network kotik_network:${BUILD_NUMBER} localhost:8082/service2:${BUILD_NUMBER}
+sudo docker run -d -p 5002:5002 --name service3 --network kotik_network:${BUILD_NUMBER} localhost:8082/service3:${BUILD_NUMBER}
+sudo docker run -d -p 5003:5003 --name service4 --network kotik_network:${BUILD_NUMBER} localhost:8082/service4:${BUILD_NUMBER}
